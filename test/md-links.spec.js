c6eb24489fd7cbc337291ed2a030b53da31c06b4
unknown <cruzyumari2@gmail.com>
2022-01-24 07:57:42 -0500

    3.-Configurando Eslint y terminando Api sin validate true

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,57 +1,55 @@
+/* eslint-disable no-undef */
 const {
   checkExistPath,
-  convertToAbsolute,
+  returnPathInAbsolute,
   checkFile,
-  readContentDir,
-  checkDirectory,
   filterFilesMD,
+  readContentDir,
+// eslint-disable-next-line import/extensions
 } = require('../lib/api.js');
 
 const pathInAbsolute = 'C:/Users/Cruz/Desktop/alexa/Codigos Js , Html, Css/cifradoCesar/md_links/LIM016-md-links/txt_prueba';
-const pathInRelative = 'txt_prueba'
+const pathInRelative = './txt_prueba';
 describe('checkExistPath', () => {
   it('deberia votar true o false', () => {
     expect(checkExistPath(pathInAbsolute)).toBe(true);
-  })
+  });
 });
 
 describe('convertToAbsolute', () => {
   it('deberia devolver la ruta convertida en absoluta', () => {
-    expect(convertToAbsolute(pathInRelative)).toBe('C:\\Users\\Cruz\\Desktop\\alexa\\Codigos Js , Html, Css\\cifradoCesar\\md_links\\LIM016-md-links\\txt_prueba');
-  })
+    expect(returnPathInAbsolute(pathInRelative)).toBe('C:\\Users\\Cruz\\Desktop\\alexa\\Codigos Js , Html, Css\\cifradoCesar\\md_links\\LIM016-md-links\\txt_prueba');
+  });
+  it('deberia devolver la ruta convertida en absoluta', () => {
+    expect(returnPathInAbsolute(pathInAbsolute)).toBe('C:/Users/Cruz/Desktop/alexa/Codigos Js , Html, Css/cifradoCesar/md_links/LIM016-md-links/txt_prueba');
+  });
 });
 
 describe('checkFile', () => {
   it('deberia devolver un boleano verfiicando si es archivo o no', () => {
     expect(checkFile('txt_prueba/prueba.md')).toBe(true);
-  })
+  });
 });
 
-describe('checkDirectory', () => {
-  it('deberia devolver un boleano verfiicando si es driectorio o no', () => {
-    expect(checkDirectory('txt_prueba')).toBe(true);
-  })
-});
 describe('readContenDirectory', () => {
   it('deberia devolver un string diciendo que eldirectorio esta vacio', () => {
-    expect(readContentDir('carpetaVacia')).toBe('Esta carpeta esta vacía')
+    expect(readContentDir('carpetaVacia')).toBe('Esta carpeta esta vacía');
   });
 
   it('deberia devolver un array con los archicos del directorio', () => {
-    expect(readContentDir('txt_prueba')).toEqual(['prueba.md', 'pruebaText.txt'])
+    expect(readContentDir('txt_prueba')).toEqual(['txt_prueba\\prueba.md', 'txt_prueba\\pruebaText.txt']);
   });
 });
 describe('filterFilesMD', () => {
   it('deberia devolver un string diciendo que el input no contiene archivos md', () => {
-    expect(filterFilesMD(readContentDir('imagenes'))).toBe('ESta carpeta no contiene archivos md')
+    expect(filterFilesMD(readContentDir('imagenes'))).toBe('La ruta ingresada no es(o no contiene) archivo(s) con extension .md');
   });
 
   it('deberia devolver un array con los archicos del directorio que tienen extension md', () => {
-    expect(filterFilesMD(readContentDir('txt_prueba'))).toEqual(['prueba.md'])
+    expect(filterFilesMD(readContentDir('txt_prueba'))).toEqual(['txt_prueba\\prueba.md']);
   });
 });
 /* const mdLinks = require('../');
-
 
 describe('mdLinks', () => {
 
--- txt_prueba/prueba.md
+++ txt_prueba/prueba.md
@@ -1,1 +1,10 @@
-hola mundo Como estas
+# joi
+
+### Visit the [joi.dev](https://joi.dev) Developer Portal for tutorials, documentation, and support
+
+## Useful resources
+
+- [Documentation and API](https://joi.dev/api/)
+- [Versions status](https://joi.dev/resources/status/#joi)
+- [Changelog](https://joi.dev/resources/changelog/)
+- [Project policies](https://joi.dev/policies/)
